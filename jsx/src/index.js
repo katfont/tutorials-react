e4e2705c72992a8483402148ec018139ed31d071
Katt Lapple <kmfontecha@ucanr.edu>
2019-04-08 20:44:12 -0700

    Property names

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
+	const buttonText = { text: "Click me" };
+	const labelText = "Enter name:";
+
 	return (
 		<div>
-			<label class="label" for="name">
-				Enter name:
+			<label className="label" htmlFor="name">
+				{labelText}
 			</label>
 			<input id="name" type="text" />
 			<button style={{ backgroundColor: "blue", color: "white" }}>
-				Submit
+				{buttonText.text}
 			</button>
 		</div>
 	);
